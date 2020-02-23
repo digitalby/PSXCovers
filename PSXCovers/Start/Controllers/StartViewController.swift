9fd3ab111eb5edbdb1cbfd65d18f7bc0775221bf
digitalby <yury@digitalby.me>
2020-02-23 18:12:07 +0300

    Add request handling stubs

--- PSXCovers/Start/Controllers/StartViewController.swift
+++ PSXCovers/Start/Controllers/StartViewController.swift
@@ -61,12 +61,47 @@ extension StartViewController: GameURLSelectorViewDelegate {
     }
 }
 
+//MARK: - Network request
+extension StartViewController {
+    func makeWaitAlert() -> UIAlertController {
+        let alert = UIAlertController(title: "Please wait, working...", message: nil, preferredStyle: .alert)
+        let throbber = UIActivityIndicatorView(style: .medium)
+        throbber.startAnimating()
+        throbber.translatesAutoresizingMaskIntoConstraints = false
+        alert.view.addSubview(throbber)
+        NSLayoutConstraint.activate([
+            throbber.centerYAnchor.constraint(equalTo: alert.view.centerYAnchor),
+            throbber.leadingAnchor.constraint(equalTo: alert.view.leadingAnchor, constant: 16.0)
+        ])
+        return alert
+    }
+
+    func makeRequest(urlString: String) {
+        let alert = makeWaitAlert()
+        present(alert, animated: true) { [unowned self] in
+            print("Request started!")
+            self.requestCompleted(waitAlert: alert)
+        }
+    }
+
+    func requestCompleted(waitAlert: UIAlertController) {
+        waitAlert.dismiss(animated: true) { [unowned self] in
+            let alert = UIAlertController(title: "Done", message: "Request completed!", preferredStyle: .alert)
+            let buttonOk = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(buttonOk)
+            self.present(alert, animated: true)
+        }
+    }
+}
+
 //MARK: - Actions
 extension StartViewController {
     @IBAction func didSelectGoBarButtonItem(_ sender: Any) {
-        let alert = UIAlertController(title: "Go", message: "You selected Go!", preferredStyle: .alert)
-        let buttonOk = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(buttonOk)
-        present(alert, animated: true)
+        guard
+            let urlString = gameURLSelectorView.gameURLField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+            !urlString.isEmpty
+        else { return }
+        gameURLSelectorView.gameURLField.resignFirstResponder()
+        makeRequest(urlString: urlString)
     }
 }
