1e23a8f4773d323da55653db208146d3b7ac9f21
digitalby <yury@digitalby.me>
2020-02-23 14:28:39 +0300

    Initial commit

---
+++ PSXCovers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PSXCovers
+//
+//  Created by Digital on 23/02/2020.
+//  Copyright © 2020 digitalby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
