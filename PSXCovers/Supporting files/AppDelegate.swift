6ec98b9cb4de1e2df9dc763b6ab1c2f5debfb537
digitalby <yury@digitalby.me>
2020-03-04 19:16:17 +0300

    Add settings bundle

--- PSXCovers/Supporting files/AppDelegate.swift
+++ PSXCovers/Supporting files/AppDelegate.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        deleteAllDataIfNeeded()
         return true
+    }
+}
+
+//MARK: - Settings bundle
+private extension AppDelegate {
+    func deleteAllDataIfNeeded() {
+        let key = "DeleteAllContent"
+        if UserDefaults.standard.bool(forKey: key) {
+            UserDefaults.standard.set(false, forKey: key)
+            let appDomain = Bundle.main.bundleIdentifier
+            UserDefaults.standard.removePersistentDomain(forName: appDomain!)
+            deleteAllData()
+        }
+    }
+}
+
+//MARK: - Data persistence helpers
+private extension AppDelegate {
+    func deleteAllData() {
+        try? DataService.realm.write {
+            DataService.realm.deleteAll()
+        }
     }
 }
 
